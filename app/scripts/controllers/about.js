fa62876b09f3db012f1faced77c167e1b93975a5
Nghia Tran <m18tran@students.latrobe.edu.au>
2015-09-25 02:06:08 +1000

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name starchamberRebornApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the starchamberRebornApp
+ */
+angular.module('starchamberRebornApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name starchamberRebornApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the starchamberRebornApp
+ */
+angular.module('starchamberRebornApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
